d99c50fd218563e13b9335ae55f136e48728a3a1
HanYoonSoo <hdbstn3055@gmail.com>
2026-04-21 16:11:05 +0900

    feat: Spring Batch DB(MongoDB) 예제

--- src/main/java/com/system/batch/db/entity/Report.java
+++ src/main/java/com/system/batch/db/entity/Report.java
@@ -1,23 +1,23 @@
-package com.system.batch.db.entity;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "reports")
-@Getter
-public class Report {
-    @Id
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
-    private Post post;
-
-    private String reportType;
-    private int reporterLevel;
-    private String evidenceData;
-    private LocalDateTime reportedAt;
-}
+//package com.system.batch.db.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "reports")
+//@Getter
+//public class Report {
+//    @Id
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "post_id")
+//    private Post post;
+//
+//    private String reportType;
+//    private int reporterLevel;
+//    private String evidenceData;
+//    private LocalDateTime reportedAt;
+//}
